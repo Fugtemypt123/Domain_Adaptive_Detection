97658155f82a08ee6aa466c24960e2136f51c6b7
ltttpku <ting_lei@pku.edu.cn>
2024-01-17 12:02:52 +0800

    ysf_baseline_0

---
+++ models/deformable_detr.py
@@ -0,0 +1,208 @@
+import math
+import copy
+
+import torch
+from torch.nn.functional import relu, interpolate
+from torch import nn
+
+
+class MLP(nn.Module):
+
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers):
+        super().__init__()
+        self.num_layers = num_layers
+        h = [hidden_dim] * (num_layers - 1)
+        self.layers = nn.ModuleList(
+            nn.Linear(n, k) for n, k in zip([input_dim] + h, h + [output_dim])
+        )
+
+    def forward(self, x):
+        for i, layer in enumerate(self.layers):
+            x = relu(layer(x)) if i < self.num_layers - 1 else layer(x)
+        return x
+
+
+class MultiConv2d(nn.Module):
+
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers):
+        super().__init__()
+        self.num_layers = num_layers
+        h = [hidden_dim] * (num_layers - 1)
+        self.layers = nn.ModuleList(
+            nn.Conv2d(n, k, kernel_size=(3, 3), padding=1) for n, k in zip([input_dim] + h, h + [output_dim])
+        )
+
+    def forward(self, x):
+        for i, layer in enumerate(self.layers):
+            x = relu(layer(x)) if i < self.num_layers - 1 else layer(x)
+        return x
+
+
+class MultiConv1d(nn.Module):
+
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers):
+        super().__init__()
+        self.num_layers = num_layers
+        h = [hidden_dim] * (num_layers - 1)
+        self.layers = nn.ModuleList(
+            nn.Conv1d(n, k, kernel_size=3) for n, k in zip([input_dim] + h, h + [output_dim])
+        )
+
+    def forward(self, x):
+        for i, layer in enumerate(self.layers):
+            x = relu(layer(x)) if i < self.num_layers - 1 else layer(x)
+        return x
+
+
+class GradReverse(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, eta=1.0):
+        ctx.eta = eta
+        return x
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return (grad_output * -ctx.eta), None
+
+
+def grad_reverse(x, eta=1.0):
+    return GradReverse.apply(x, eta)
+
+def inverse_sigmoid(x, eps=1e-5):
+    x = x.clamp(min=0, max=1)
+    x1 = x.clamp(min=eps)
+    x2 = (1 - x).clamp(min=eps)
+    return torch.log(x1/x2)
+
+
+class DeformableDETR(nn.Module):
+
+    def __init__(self, backbone, position_encoding, transformer, num_classes, num_queries, num_feature_levels):
+        super().__init__()
+        self.backbone = backbone
+        self.position_encoding = position_encoding
+        self.num_queries = num_queries
+        self.transformer = transformer
+        hidden_dim = transformer.hidden_dim
+        self.class_embed = nn.Linear(hidden_dim, num_classes)
+        self.bbox_embed = MLP(hidden_dim, hidden_dim, 4, 3)
+        self.num_feature_levels = num_feature_levels
+        self.query_embed = nn.Embedding(num_queries, hidden_dim*2)
+
+        num_backbone_outs = backbone.num_outputs
+        input_proj_list = []
+        for _ in range(num_backbone_outs):
+            in_channels = backbone.get_backbone()[1][_]
+            input_proj_list.append(nn.Sequential(
+                nn.Conv2d(in_channels, hidden_dim, kernel_size=1),
+                nn.GroupNorm(32, hidden_dim),
+            ))
+        for _ in range(num_feature_levels - num_backbone_outs):
+            input_proj_list.append(nn.Sequential(
+                nn.Conv2d(in_channels, hidden_dim, kernel_size=3, stride=2, padding=1),
+                nn.GroupNorm(32, hidden_dim),
+            ))
+            in_channels = hidden_dim
+        self.input_proj = nn.ModuleList(input_proj_list)
+
+        prior_prob = 0.01
+        bias_value = -math.log((1 - prior_prob) / prior_prob)
+        self.class_embed.bias.data = torch.ones(num_classes) * bias_value
+        nn.init.constant_(self.bbox_embed.layers[-1].weight.data, 0)
+        nn.init.constant_(self.bbox_embed.layers[-1].bias.data, 0)
+        for proj in self.input_proj:
+            nn.init.xavier_uniform_(proj[0].weight, gain=1)
+            nn.init.constant_(proj[0].bias, 0)
+
+        # if two-stage, the last class_embed and bbox_embed is for region proposal generation
+        num_pred = transformer.decoder.num_layers
+
+        nn.init.constant_(self.bbox_embed.layers[-1].bias.data[2:], -2.0)
+        self.class_embed = nn.ModuleList([self.class_embed for _ in range(num_pred)])
+        self.bbox_embed = nn.ModuleList([self.bbox_embed for _ in range(num_pred)])
+        self.transformer.decoder.bbox_embed = None
+    
+    def forward(self, x, ms):
+        """ The forward expects a NestedTensor, which consists of:
+               - samples.tensor: batched images, of shape [batch_size x 3 x H x W]
+               - samples.mask: a binary mask of shape [batch_size x H x W], containing 1 on padded pixels
+
+            It returns a dict with the following elements:
+               - "pred_logits": the classification logits (including no-object) for all queries.
+                                Shape= [batch_size x num_queries x (num_classes + 1)]
+               - "pred_boxes": The normalized boxes coordinates for all queries, represented as
+                               (center_x, center_y, height, width). These values are normalized in [0, 1],
+                               relative to the size of each individual image (disregarding possible padding).
+                               See PostProcess for information on how to retrieve the unnormalized bounding box.
+               - "aux_outputs": Optional, only returned when auxilary losses are activated. It is a list of
+                                dictionnaries containing the two above keys for each decoder layer.
+        """
+        # backbone and mask
+        features = self.backbone(x)
+        masks = []
+        for x in features:
+            m = ms
+            assert m is not None
+            masks.append(interpolate(m[None].float(), size=x.shape[-2:]).to(torch.bool)[0])
+
+        # positional encoding
+        pos = []
+        for l in range(len(features)):
+            pos.append(self.position_encoding(features[l], masks[l]).to(features[l].dtype))
+
+        # input projection
+        srcs = []
+        for l in range(len(features)):
+            srcs.append(self.input_proj[l](features[l]))
+    
+        # add last feature level
+        if self.num_feature_levels > len(srcs):
+            _len_srcs = len(srcs)
+            for l in range(_len_srcs, self.num_feature_levels):
+                if l == _len_srcs:
+                    src = self.input_proj[l](features[-1])
+                else:
+                    src = self.input_proj[l](srcs[-1])
+                m = ms
+                mask = interpolate(m[None].float(), size=src.shape[-2:]).to(torch.bool)[0]
+                pos_l = self.position_encoding(src, mask).to(src.dtype)
+                srcs.append(src)
+                masks.append(mask)
+                pos.append(pos_l)
+
+        query_embeds = self.query_embed.weight
+        hs, init_reference, inter_references = self.transformer(srcs, masks, pos, query_embeds)
+
+        outputs_classes = []
+        outputs_coords = []
+        for lvl in range(hs.shape[0]):
+            if lvl == 0:
+                reference = init_reference
+            else:
+                reference = inter_references[lvl - 1]
+            reference = inverse_sigmoid(reference)
+            outputs_class = self.class_embed[lvl](hs[lvl])
+            tmp = self.bbox_embed[lvl](hs[lvl])
+            # print(f"lvl:{lvl}, outputs_bbox:{tmp}")
+            if reference.shape[-1] == 4:
+                tmp += reference
+            else:
+                assert reference.shape[-1] == 2
+                tmp[..., :2] += reference
+                # print(f"lvl:{lvl}, tmp.shape:{tmp.shape}")
+            outputs_coord = tmp.sigmoid()
+            # print(f"lvl:{lvl}, outputs_class:{outputs_class}")
+            
+            outputs_classes.append(outputs_class)
+            outputs_coords.append(outputs_coord)
+        outputs_class = torch.stack(outputs_classes)
+        outputs_coord = torch.stack(outputs_coords)
+
+        # print(outputs_class.shape)
+        # print(outputs_coord.shape)
+        out = {'logits_all': outputs_class, 'boxes_all': outputs_coord, 'pred_logits': outputs_class[-1], 'pred_boxes': outputs_coord[-1]}
+        # print(out['pred_logits'].shape)
+        # print(out['pred_boxes'].shape)
+        return out
+
+
---
+++ models/deformable_transformer.py
@@ -0,0 +1,276 @@
+import copy
+import math
+
+import torch
+from torch.nn.functional import relu, gelu, glu
+from torch import nn
+from torch.nn.init import xavier_uniform_, constant_, normal_
+
+from models.ops.modules import MSDeformAttn
+
+
+def inverse_sigmoid(x, eps=1e-5):
+    x = x.clamp(min=0, max=1)
+    x1 = x.clamp(min=eps)
+    x2 = (1 - x).clamp(min=eps)
+    return torch.log(x1 / x2)
+
+
+class DeformableTransformer(nn.Module):
+    def __init__(self, hidden_dim, num_heads, num_encoder_layers, num_decoder_layers, feedforward_dim, dropout):
+        super().__init__()
+        
+        self.hidden_dim = hidden_dim
+        self.num_heads = num_heads
+
+        encoder_layer = DeformableTransformerEncoderLayer(hidden_dim, num_heads, feedforward_dim, dropout)
+        self.encoder = DeformableTransformerEncoder(encoder_layer, num_encoder_layers)
+
+        decoder_layer = DeformableTransformerDecoderLayer(hidden_dim, num_heads, feedforward_dim, dropout)
+        self.decoder = DeformableTransformerDecoder(decoder_layer, num_decoder_layers)
+
+        self.level_embed = nn.Parameter(torch.Tensor(4, hidden_dim))
+        self.reference_points = nn.Linear(hidden_dim, 2)
+
+        self._reset_parameters()
+        return
+
+    def _reset_parameters(self):
+        for p in self.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
+        for m in self.modules():
+            if isinstance(m, MSDeformAttn):
+                m.reset_parameters() # 这里不好说会不会有问题，原来是_reset_parameters
+        xavier_uniform_(self.reference_points.weight.data, gain=1.0)
+        constant_(self.reference_points.bias.data, 0.)
+        normal_(self.level_embed)
+
+    def get_valid_ratio(self, mask):
+        _, H, W = mask.shape
+        valid_H = torch.sum(~mask[:, :, 0], 1)
+        valid_W = torch.sum(~mask[:, 0, :], 1)
+        valid_ratio_h = valid_H.float() / H
+        valid_ratio_w = valid_W.float() / W
+        valid_ratio = torch.stack([valid_ratio_w, valid_ratio_h], -1)
+        return valid_ratio
+
+    def forward(self, srcs, masks, pos_embeds, query_embed=None):
+        assert query_embed is not None
+
+        # prepare input for encoder
+        src_flatten = []
+        mask_flatten = []
+        lvl_pos_embed_flatten = []
+        spatial_shapes = []
+        for lvl, (src, mask, pos_embed) in enumerate(zip(srcs, masks, pos_embeds)):
+            bs, c, h, w = src.shape
+            # print(f"src.shape: {src.shape}") 
+            spatial_shape = (h, w)
+            spatial_shapes.append(spatial_shape)
+            src = src.flatten(2).transpose(1, 2)
+            mask = mask.flatten(1)
+            pos_embed = pos_embed.flatten(2).transpose(1, 2)
+            # print(f"pos_embed.shape: {pos_embed.shape}")
+            # print(f"level_embed.shape: {self.level_embed[lvl].shape}")
+            lvl_pos_embed = pos_embed + self.level_embed[lvl].view(1, 1, -1)
+            lvl_pos_embed_flatten.append(lvl_pos_embed)
+            src_flatten.append(src)
+            mask_flatten.append(mask)
+        src_flatten = torch.cat(src_flatten, 1)
+        mask_flatten = torch.cat(mask_flatten, 1)
+        lvl_pos_embed_flatten = torch.cat(lvl_pos_embed_flatten, 1)
+        spatial_shapes = torch.as_tensor(spatial_shapes, dtype=torch.long, device=src_flatten.device)
+        level_start_index = torch.cat((spatial_shapes.new_zeros((1, )), spatial_shapes.prod(1).cumsum(0)[:-1]))
+        valid_ratios = torch.stack([self.get_valid_ratio(m) for m in masks], 1)
+
+        # encoder
+        memory = self.encoder(src_flatten, spatial_shapes, level_start_index, valid_ratios, lvl_pos_embed_flatten, mask_flatten)
+
+        # prepare input for decoder
+        bs, _, c = memory.shape
+        
+        query_embed, tgt = torch.split(query_embed, c, dim=1)
+        query_embed = query_embed.unsqueeze(0).expand(bs, -1, -1)
+        tgt = tgt.unsqueeze(0).expand(bs, -1, -1)
+        reference_points = self.reference_points(query_embed).sigmoid()
+        init_reference_out = reference_points
+
+        # decoder
+        hs, inter_references = self.decoder(tgt, reference_points, memory,
+                                            spatial_shapes, level_start_index, valid_ratios, query_embed, mask_flatten)
+
+        inter_references_out = inter_references
+        return hs, init_reference_out, inter_references_out
+
+
+class DeformableTransformerEncoderLayer(nn.Module):
+
+    def __init__(self, hidden_dim, num_heads, feedforward_dim, dropout):
+        super().__init__()
+
+        # self attention
+        self.self_attn = MSDeformAttn(d_model=hidden_dim, n_levels=4, n_heads=num_heads, n_points=4)
+        self.dropout1 = nn.Dropout(dropout)
+        self.norm1 = nn.LayerNorm(hidden_dim)
+
+        # ffn
+        self.linear1 = nn.Linear(hidden_dim, feedforward_dim)
+        self.activation = nn.ReLU()
+        self.dropout2 = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(feedforward_dim, hidden_dim)
+        self.dropout3 = nn.Dropout(dropout)
+        self.norm2 = nn.LayerNorm(hidden_dim)
+
+    @staticmethod
+    def with_pos_embed(tensor, pos):
+        return tensor if pos is None else tensor + pos
+
+    def forward_ffn(self, src):
+        src2 = self.linear2(self.dropout2(self.activation(self.linear1(src))))
+        src = src + self.dropout3(src2)
+        src = self.norm2(src)
+        return src
+
+    def forward(self, src, pos, reference_points, spatial_shapes, level_start_index, padding_mask=None):
+        # self attention
+        src2 = self.self_attn(self.with_pos_embed(src, pos), reference_points, src, spatial_shapes, level_start_index, padding_mask)
+        src = src + self.dropout1(src2)
+        src = self.norm1(src)
+
+        # ffn
+        src = self.forward_ffn(src)
+        return src
+
+
+class DeformableTransformerEncoder(nn.Module):
+
+    def __init__(self, encoder_layer, num_layers):
+        super().__init__()
+        self.layers = _get_clones(encoder_layer, num_layers)
+        self.num_layers = num_layers
+
+    @staticmethod
+    def get_reference_points(spatial_shapes, valid_ratios, device):
+        reference_points_list = []
+        for lvl, (H_, W_) in enumerate(spatial_shapes):
+
+            ref_y, ref_x = torch.meshgrid(torch.linspace(0.5, H_ - 0.5, H_, dtype=torch.float32, device=device),
+                                          torch.linspace(0.5, W_ - 0.5, W_, dtype=torch.float32, device=device))
+            ref_y = ref_y.reshape(-1)[None] / (valid_ratios[:, None, lvl, 1] * H_)
+            ref_x = ref_x.reshape(-1)[None] / (valid_ratios[:, None, lvl, 0] * W_)
+            ref = torch.stack((ref_x, ref_y), -1)
+            reference_points_list.append(ref)
+        reference_points = torch.cat(reference_points_list, 1)
+        reference_points = reference_points[:, :, None] * valid_ratios[:, None]
+        return reference_points
+
+    def forward(self, src, spatial_shapes, level_start_index, valid_ratios, pos=None, padding_mask=None):
+        output = src
+        reference_points = self.get_reference_points(spatial_shapes, valid_ratios, device=src.device)
+        for _, layer in enumerate(self.layers):
+            output = layer(output, pos, reference_points, spatial_shapes, level_start_index, padding_mask)
+
+        return output
+
+
+class DeformableTransformerDecoderLayer(nn.Module):
+
+    def __init__(self, hidden_dim, num_heads, feedforward_dim, dropout):
+        # d_model=256, d_ffn=1024,dropout=0.1, activation="relu",n_levels=4, n_heads=8, n_points=4):
+        super().__init__()
+
+        # cross attention
+        self.cross_attn = MSDeformAttn(d_model=hidden_dim, n_levels=4, n_heads=num_heads, n_points=4)
+        self.dropout1 = nn.Dropout(dropout)
+        self.norm1 = nn.LayerNorm(hidden_dim)
+
+        # self attention
+        self.self_attn = nn.MultiheadAttention(hidden_dim, num_heads, dropout=dropout)
+        self.dropout2 = nn.Dropout(dropout)
+        self.norm2 = nn.LayerNorm(hidden_dim)
+
+        # ffn
+        self.linear1 = nn.Linear(hidden_dim, feedforward_dim)
+        self.activation = nn.ReLU()
+        self.dropout3 = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(feedforward_dim, hidden_dim)
+        self.dropout4 = nn.Dropout(dropout)
+        self.norm3 = nn.LayerNorm(hidden_dim)
+
+    @staticmethod
+    def with_pos_embed(tensor, pos):
+        return tensor if pos is None else tensor + pos
+
+    def forward_ffn(self, tgt):
+        tgt2 = self.linear2(self.dropout3(self.activation(self.linear1(tgt))))
+        tgt = tgt + self.dropout4(tgt2)
+        tgt = self.norm3(tgt)
+        return tgt
+
+    def forward(self, tgt, query_pos, reference_points, src, src_spatial_shapes, level_start_index, src_padding_mask=None):
+        # self attention
+        q = k = self.with_pos_embed(tgt, query_pos)
+        tgt2 = self.self_attn(q.transpose(0, 1), k.transpose(0, 1), tgt.transpose(0, 1))[0].transpose(0, 1)
+        tgt = tgt + self.dropout2(tgt2)
+        tgt = self.norm2(tgt)
+
+        # cross attention
+        tgt2 = self.cross_attn(self.with_pos_embed(tgt, query_pos),
+                               reference_points,
+                               src, src_spatial_shapes, level_start_index, src_padding_mask)
+        tgt = tgt + self.dropout1(tgt2)
+        tgt = self.norm1(tgt)
+
+        # ffn
+        tgt = self.forward_ffn(tgt)
+
+        return tgt
+    
+
+class DeformableTransformerDecoder(nn.Module):
+
+    def __init__(self, decoder_layer, num_layers):
+        super().__init__()
+        self.layers = _get_clones(decoder_layer, num_layers)
+        self.num_layers = num_layers
+        # hack implementation for iterative bounding box refinement and two-stage Deformable DETR
+        self.bbox_embed = None
+        self.class_embed = None
+
+    def forward(self, tgt, reference_points, src, src_spatial_shapes, src_level_start_index, src_valid_ratios,
+                query_pos=None, src_padding_mask=None):
+        output = tgt
+
+        intermediate = []
+        intermediate_reference_points = []
+        for lid, layer in enumerate(self.layers):
+            if reference_points.shape[-1] == 4:
+                reference_points_input = reference_points[:, :, None] \
+                                         * torch.cat([src_valid_ratios, src_valid_ratios], -1)[:, None]
+            else:
+                assert reference_points.shape[-1] == 2
+                reference_points_input = reference_points[:, :, None] * src_valid_ratios[:, None]
+            output = layer(output, query_pos, reference_points_input, src, src_spatial_shapes, src_level_start_index, src_padding_mask)
+
+            # hack implementation for iterative bounding box refinement
+            if self.bbox_embed is not None:
+                tmp = self.bbox_embed[lid](output)
+                if reference_points.shape[-1] == 4:
+                    new_reference_points = tmp + inverse_sigmoid(reference_points)
+                    new_reference_points = new_reference_points.sigmoid()
+                else:
+                    assert reference_points.shape[-1] == 2
+                    new_reference_points = tmp
+                    new_reference_points[..., :2] = tmp[..., :2] + inverse_sigmoid(reference_points)
+                    new_reference_points = new_reference_points.sigmoid()
+                reference_points = new_reference_points.detach()
+            
+            intermediate.append(output)
+            intermediate_reference_points.append(reference_points)
+
+        return torch.stack(intermediate), torch.stack(intermediate_reference_points)
+
+
+def _get_clones(module, N):
+    return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
